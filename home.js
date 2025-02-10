3a6a241e527c2d9e2cbd8bb6b6b71c3eed6965e8
Jai-Prakash278 <jay065117@gmail.com>
2025-02-10 21:38:43 +0530

    extra

--- home.js
+++ home.js
@@ -1,7 +1,27 @@
 const hamburger = document.querySelector('#ham');
 const LinksEle = document.querySelector('#hidden');
+const cutEle = document.getElementById('cross');
+const searchEle = document.querySelector('#search');
+const hideEle = document.querySelector('#hide-btn');
+const searchbtn = document.querySelector('.search-btn');
 
 
 hamburger.addEventListener('click', () => {
-    LinksEle.style.display = 'block';
+    LinksEle.style.display = 'flex';
+    hamburger.style.display = "none";
+    searchEle.style.display = 'none';
+    searchbtn.style.display = 'block';
+});
+
+cutEle.addEventListener('click', () => {
+    LinksEle.style.display = "none";
+    hamburger.style.display = "block";
+    searchEle.style.display = 'flex';
+    // hideEle.style.display = 'block';
+});
+
+hideEle.addEventListener('click', () => {
+    LinksEle.style.display = 'flex';
+    // hideEle.style.display = 'none';
+    searchbtn.style.display = 'block';
 });
